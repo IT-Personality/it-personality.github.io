5aaec648593c8e82a89626be1ce8465b1280967d
neurobeast1 <92996537+IT-Personality@users.noreply.github.com>
2022-09-11 19:02:00 +0300

    KlondikeITBlogger

---
+++ windows/main_programs/reg/js/mi.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+ var link = $("#link"),
+     counter = $("#counter");
+ link.on('click', function(e) {
+  e.preventDefault();
+  var ajax = $.ajax({
+   method : 'post',
+   url : 'like.php',
+   dataType : 'text',
+   data : { 'count' : 1 }
+  });
+  ajax.done(function(data){
+   data = JSON.parse(data);
+   counter.html(parseInt(data));
+  }) ;
+ });
+});
+
+function OnLoad() {
+            var subm = document.getElementById("link");
+
+            subm.onclick = function() {
+                localStorage["submit33"]++;
+                document.getElementById('link').style.opacity=0;
+                document.getElementById('like').style.opacity=0;
+                alert("Спасибо за ваш лайк!");
+            }
+
+            if( localStorage.getItem("submit33") === null ) {
+                localStorage.setItem("submit33", 0);
+            } else if (localStorage["submit33"] >= 1) {
+                subm.disabled = true;
+                document.getElementById('link').style.backgroundColor= '#818181';
+            }
+};
+        window.onload = OnLoad;
