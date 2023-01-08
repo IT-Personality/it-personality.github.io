da23f5c0b68dc5ce6240796dfffb2214116637e3
neurobeast1 <92996537+IT-Personality@users.noreply.github.com>
2023-01-08 12:25:14 +0200

    KlondikeITBlogger

---
+++ it-personality/main-it-personality.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){
+    var typed = new Typed(".type", {
+        strings: ["Full-Stack Developer", "KlondikeITBlogger", "Оказание различных услуг", "HTML, CSS, JS, PHP, SQL, Batch, Python"],
+        typeSpeed: 80,
+        backSpeed: 30,
+        loop: true
+    });
+});
