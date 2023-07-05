12103dc3327e41f2e8643e45dd6b5bf52f53d8d4
IŦ-Pers๏nality <92996537+IT-Personality@users.noreply.github.com>
2023-07-05 18:42:56 +0300

    Delete it-personality directory

--- it-personality/main-it-personality.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-    var typed = new Typed(".type", {
-        strings: ["Full-Stack Developer", "KlondikeITBlogger", "Оказание различных услуг", "HTML, CSS, JS, PHP, SQL, Batch, Python"],
-        typeSpeed: 80,
-        backSpeed: 30,
-        loop: true
-    });
-});
